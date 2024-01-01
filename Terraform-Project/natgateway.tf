fa933aded5e9fc9b8163b4116f7670bd4bba0fbc
BARAKAT IDRIS <135038657+Mubarokahh@users.noreply.github.com>
2024-01-01 19:38:47 +0000

    Update natgateway.tf

--- Terraform-Project/natgateway.tf
+++ Terraform-Project/natgateway.tf
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5d84e2cd35316ebafd7afa4619138f19a8d3a3be11a91148a54b603d3e101aad
-size 473
+resource "aws_eip" "nat_eip" {
+  vpc = true
+
+  depends_on = [aws_internet_gateway.igw]
+ tags = merge(
+    var.tags,
+    {
+      Name = format("%s-NATEIP-%s",var.name,var.environment)
+    } 
+  ) 
+
+}
+
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public.*.id,0)
+  depends_on = [aws_internet_gateway.igw]
+ tags = merge(
+    var.tags,
+    {
+      Name = format("%s-NATGW-%s",var.name,var.environment)
+    } 
+  )
+}
