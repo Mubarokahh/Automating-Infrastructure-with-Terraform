427d89fabe0ce5ce1b1e61051be08b939429f46b
BARAKAT IDRIS <135038657+Mubarokahh@users.noreply.github.com>
2024-01-01 19:37:44 +0000

    Update internet_gateway.tf

--- Terraform-Project/internet_gateway.tf
+++ Terraform-Project/internet_gateway.tf
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8232d839e5d4ae1364faae748250f3e2d1573326f053ed9f2b4ad365a00c2cc2
-size 196
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = merge(
+        var.tags,
+        {
+            Name = format("%s-%s!", aws_vpc.main.id,"IG")
+        }
+
+    )
+
+  
+}
