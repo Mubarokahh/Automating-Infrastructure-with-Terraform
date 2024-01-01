bb5e34761d8599195cb511b672fc7892a5d0301a
BARAKAT IDRIS <135038657+Mubarokahh@users.noreply.github.com>
2024-01-01 19:39:49 +0000

    Update rds.tf

--- Terraform-Project/rds.tf
+++ Terraform-Project/rds.tf
@@ -1,3 +1,32 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e1d5cb78ceb3b1d2bba15e88d7a0814743a47969be5e149f7cc5a394479da1b0
-size 926
+
+# This section will create the subnet group for the RDS instance using the private subnet
+resource "aws_db_subnet_group" "ACS-rds" {
+  name       = "acs-rds"
+  subnet_ids = [aws_subnet.private[2].id, aws_subnet.private[3].id]
+
+ tags = merge(
+    var.tags,
+    {
+      Name = "ACS-rds"
+    },
+  )
+}
+
+
+# create the RDS instance with the subnets group
+resource "aws_db_instance" "ACS-rds" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  db_name                   = "barakatdb"
+  username               = var.db-username
+  password               = var.db-password
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.ACS-rds.name
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.datalayer-sg.id]
+  multi_az               = "true"
+}
+
