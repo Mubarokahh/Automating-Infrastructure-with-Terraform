da78470b99b5c3c77527cb8b10bf30edc0f087f2
BARAKAT IDRIS <135038657+Mubarokahh@users.noreply.github.com>
2024-01-01 19:38:15 +0000

    Update main.tf

--- Terraform-Project/main.tf
+++ Terraform-Project/main.tf
@@ -1,3 +1,56 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:543174c132cc6cf06cc93c39e3b8df81767e59d9f06a209abf9cd7218df1fb1e
-size 1463
+
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+state = "available"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support 
+  enable_dns_hostnames           = var.enable_dns_support
+  
+
+}
+
+# Create public subnets
+ resource "aws_subnet" "public" {
+  count  = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets   
+  vpc_id = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-PublicSubnet-%s",var.name,count.index)
+    } 
+  )
+
+}
+
+ 
+  # Create Private Subnet
+ 
+  resource "aws_subnet" "Private" {
+  count  = var.preferred_number_of_private_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_private_subnets   
+  vpc_id = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8 , count.index + 2)
+  map_public_ip_on_launch = true 
+  availability_zone       = data.aws_availability_zones.available.names[count.index % 2]
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-PrivateSubnet-%s",var.name,count.index)
+    } 
+  )
+}
+
+ 
+
