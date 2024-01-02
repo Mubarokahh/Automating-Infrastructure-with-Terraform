0e5af55cdb3892f9c711c7d5cc46ce880e51d25d
BARAKAT IDRIS <135038657+Mubarokahh@users.noreply.github.com>
2024-01-02 12:04:40 +0000

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,14 @@
-
+  resource "aws_s3_bucket" "terraform_state" {
+  bucket = "dele-dev-terraform-bucket-2"
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
